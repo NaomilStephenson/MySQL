ef144cbcfbc6afeb950c97eeffa6a10593dbded7
Naomi <Naomilstepenson@gmail.com>
2023-06-23 14:32:48 +0100

    Amended Add Customer Files to include option to delete a record

--- AddAddress.sql
+++ AddAddress.sql
@@ -1,9 +1,5 @@
 USE GameStore;
 INSERT INTO Address (HouseName,Street,Street2,Town,County,PostCode)
-Values
-(42,'London St','','Bristol','Somerset','BS15 07HG'),
-('The Platform','Gable Close','Gable Cross','Oxford','Oxfordshire','OX92 06BU'),
-(7,'Station Rd','','Bridgewater','Somerset','BS15 07HG'),
-(104,'Law Courts','Westbridge Park','Newbury','Wiltshire','TR17 16JK')
-;
+Values (150,'St Johns Way','','Kingston','Essex','ES55 13RH');
+-- DELETE FROM Address WHERE Criteria = Value ;
 SELECT * FROM Address;
